e2df130d13aaea2c9a54655519355867ba495716
SACRELEE <sacrelee@gmail.com>
2015-05-30 20:27:53 +0800

    Initial Commit

---
+++ Demo_TextKitPaging/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo_TextKitPaging
+//
+//  Created by SACRELEE on 15/5/30.
+//  Copyright (c) 2015年 Sumtice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Demo_TextKitPagingTests/Demo_TextKitPagingTests.m
@@ -0,0 +1,40 @@
+//
+//  Demo_TextKitPagingTests.m
+//  Demo_TextKitPagingTests
+//
+//  Created by SACRELEE on 15/5/30.
+//  Copyright (c) 2015年 Sumtice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Demo_TextKitPagingTests : XCTestCase
+
+@end
+
+@implementation Demo_TextKitPagingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
